ceb9518ecab0fd23d49afc87f7e491d018b9488c
Hoa <hoa@Hoas-MBP.fritz.box>
2021-01-03 22:22:54 +0100

    check out prod

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import { hello } from './hello';
 import './styles/main.scss';
+import Scrollmagic from 'scrollmagic';
 
-document.querySelector('h1').textContent = hello('How are you?');
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+    duration: 500, // the scene should last for a scroll distance of 100px
+    offset: 50 // start this scene after scrolling for 50px
+})
+    .setPin('body') // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
+
+    document.querySelector('h1').textContent = hello('How are you?');
